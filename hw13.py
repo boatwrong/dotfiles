ba27b24933aa9c874038428a566df5a0337358e3
boatwrong <bryantboatright@ou.edu>
2022-04-22 08:26:02 -0500

    transfer file

---
+++ hw13.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+tInf = 25 # Celsius
+t0 = 100 # Celsius
+d = 0.01 # diameter (m)
+L = 0.1 # height (m)
+k = 401 # W/mK
+h - 15 # W/m^2 K
+P = np.pi * d # Perimiter (m)
+
+M = h * P / (k * Ac)
+m = pow(m, 0.5)
+
+numerator = np.cosh(L - x) + (h / (m * k)) * np.sinh(m * (L - x))
+denominator = np.cosh(m * L) + (h / (m * k) * np.sinh(m * L))
+
