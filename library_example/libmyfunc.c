76495c7301c77228c8b90bd7011784d0620dc426
boatwrong <bryantboatright@icloud.com>
2022-12-20 12:22:10 -0600

    clean up extra files

--- library_example/libmyfunc.c
+++
@@ -1,8 +0,0 @@
-#include"mylib.h"
-#include <stdio.h>
-
-void poop(char *name)
-{
-    if(name != NULL)
-    printf("%s IS POOPIN!\n", name);
-}
--- library_example/mylib.h
+++
@@ -1,6 +0,0 @@
-#ifndef _MYLIB_H_
-#define _MYLIB_H_
-
-void poop();
-
-#endif
